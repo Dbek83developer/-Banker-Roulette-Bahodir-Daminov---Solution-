fdbcd0acaf6944b4263d6ac5b7070e46f9f5e203
Bahodir <dbek.developer@gmail.com>
2022-02-12 07:24:53 +0000

    add \n

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 # Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
+names_string = input("Give me everybody's names, separated by a comma. \n")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 
